5925cdb401054e655aad49d2fe237298b70f0b65
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-28 06:57:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
- var lowercase = "hello"
- string.toLowerCase() === lowercase
- if(string === lowercase) {
+  if(string === lowercase) {
    return('I can\'t hear you!');
  }
 }
